13177504ef06938ea33dba01c4f8856662cb9b2e
dukenv0307 <dukenv0307@gmail.com>
2024-03-22 13:42:00 +0700

    chore

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
 
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
-console.log('test :>> ');
 additionalAppSetup();
